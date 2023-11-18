6d0280bb9cc75e2d1af7948c4dd083e54e0a8568
dicklant <dicklant@hotmail.com>
2023-11-18 23:46:14 +0100

    Init project

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const port = normalizePort(process.env.PORT || '3000');
+
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+app.get("/", function (req, res) {
+    res.send("Hello World!");
+});
+
+app.get("/admin", function (req, res) {
+    res.send("Admin section...");
+});
+
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`);
+});
+
+
